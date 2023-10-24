617140154b7feacab19c1136deca6f401578f63b
Niaz Ahmed <itsniaz@gmail.com>
2023-10-24 21:24:43 +0600

    first commit

---
+++ AlgoExpert.io/Tournament_Winner.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class Program {
+    public String tournamentWinner(
+        ArrayList<ArrayList<String>> competitions,
+        ArrayList<Integer> results
+    ) {
+        HashMap<String, Integer> teamWins = new HashMap<>();
+        String winningMaxTeam = "";
+        int maxWins = 0;
+
+        for (int i = 0; i < competitions.size(); i++) {
+            ArrayList<String> competition = competitions.get(i);
+            String homeTeam = competition.get(0);
+            String awayTeam = competition.get(1);
+            int winner = results.get(i);
+
+            String winningTeam = winner == 1 ? homeTeam : awayTeam;
+            teamWins.put(winningTeam, teamWins.getOrDefault(winningTeam, 0) + 3);
+
+            if (teamWins.get(winningTeam) > maxWins) {
+                winningMaxTeam = winningTeam;
+                maxWins = teamWins.get(winningTeam);
+            }
+        }
+
+        return winningMaxTeam;
+    }
+}
